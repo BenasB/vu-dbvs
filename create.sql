151a6b9e0de77fbd0be6f8e999e9915956e4c3b4
BenasB <29711974+BenasB@users.noreply.github.com>
2022-12-09 00:50:41 +0200

    Added views

--- create.sql
+++ create.sql
@@ -50,6 +50,28 @@ CREATE TABLE test_results
     test_case_id int NOT NULL REFERENCES test_cases(id) ON DELETE CASCADE
 );
 
+-- Create views
+
+CREATE VIEW leaderboard_points AS
+SELECT username, points
+FROM users
+ORDER BY points DESC, username; 
+
+CREATE VIEW leaderboard_solutions AS
+WITH successful_solutions(solution_id) AS
+(
+    SELECT solution_id
+    FROM test_results
+    GROUP BY solution_id
+    HAVING COUNT(CASE WHEN is_success='false' THEN 1 END) = 0
+)
+SELECT C.username, COUNT(B.submitted_by) AS "correct solutions"
+FROM successful_solutions AS A
+JOIN solutions AS B ON A.solution_id = B.id
+RIGHT OUTER JOIN users AS C ON B.submitted_by = C.username
+GROUP BY B.submitted_by, C.username
+ORDER BY COUNT(B.submitted_by) DESC, C.username;
+
 -- Populate data
 
 INSERT INTO users VALUES
@@ -70,11 +92,23 @@ INSERT INTO test_cases VALUES
 (DEFAULT, 'labaiIlgasZodis', 'sidoZsaglIiabal', 2);
 
 INSERT INTO solutions VALUES
-(DEFAULT, DEFAULT, '#include <stdio.h>\nint main() {\n   printf("Hello, World!");\n   return 0;\n}\n', 1, 'benasb');
+(DEFAULT, DEFAULT, '#include <stdio.h>\nint main() {\n   printf("Hello, World!");\n   return 0;\n}\n', 1, 'benasb'),
+(DEFAULT, DEFAULT, 'teisingas kodas', 1, 'benasb'),
+(DEFAULT, DEFAULT, 'printf("hi");', 2, 'benasb'),
+(DEFAULT, DEFAULT, 'mano irgi teisingas source code', 1, 'vardis123');
+
 
 INSERT INTO test_results VALUES
 (DEFAULT, 'true', 1.566, 123, 1, 1),
-(DEFAULT, 'false', 3.566, 54, 1, 2);
+(DEFAULT, 'false', 3.566, 54, 1, 2),
+(DEFAULT, 'true', 2.1, 243, 2, 1),
+(DEFAULT, 'true', 1.23, 133, 2, 2),
+(DEFAULT, 'true', 2, 112, 3, 3),
+(DEFAULT, 'true', 1, 87, 3, 4),
+(DEFAULT, 'true', 2, 112, 3, 5),
+(DEFAULT, 'true', 1, 87, 3, 6),
+(DEFAULT, 'true', 1.64, 456, 4, 1),
+(DEFAULT, 'true', 1.42, 333, 4, 2);
 
 -- Show initial data
 SELECT * FROM users;
